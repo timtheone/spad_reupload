c74da2badb570ed4549915dcf2e6d513e81e728f
Tim Almamedov <tim.almamedov@gmail.com>
2017-06-15 13:58:36 +0200

    fixed picture pagination

--- app/uploaders/receipt_uploader.rb
+++ app/uploaders/receipt_uploader.rb
@@ -2,12 +2,10 @@ class ReceiptUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
   include CarrierWave::MiniMagick
 
-  version :standart do
-  process :resize_to_fit => [700, 800]
-  end
 
- version :standart_transform do
-    cloudinary_transformation width: 700, crop: :scale, format: 'jpg'
+
+  version :standart_transform do
+    cloudinary_transformation width: 600, crop: :limit, format: 'jpg'
   end
 
 end
